73603add8e3b9e3a467f6013440a9f78fe543f36
DaMan56100 <samcraft792@gmail.com>
2022-07-09 17:01:12 +0100

    successfully connect to kotlin

--- src/main/java/net/dfplots/plotnet/mixin/ConnectMixin.java
+++ src/main/java/net/dfplots/plotnet/mixin/ConnectMixin.java
@@ -7,12 +7,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.dfplots.plotnet.PlotNetKt;
+
 @Mixin(MultiplayerScreen.class)
 // Mixins HAVE to be written in java due to constraints in the mixin system.
 public class ConnectMixin {
     @Inject(at = @At("HEAD"), method = "connect(Lnet/minecraft/client/network/ServerInfo;)V")
     private void init(ServerInfo serverInfo, CallbackInfo info) {
-        System.out.println("You just tried to connect to a server!");
-        System.out.println(serverInfo.address);
+        PlotNetKt.logConnection(serverInfo.address);
     }
 }
---
+++ src/main/kotlin/net/dfplots/plotnet/PlotNet.kt
@@ -0,0 +1,9 @@
+package net.dfplots.plotnet
+
+// For support join https://discord.gg/v6v4pMv
+
+@Suppress("unused")
+fun logConnection(ip: String) {
+    println(ip)
+}
+
